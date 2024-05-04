9697f273016d283b9a7cb72591df19632ca8a50f
Miruna Georgescu <mirubi12@gmail.com>
2024-05-04 21:03:38 +0300

    Add reviewModel

---
+++ models/reviewModel.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Review = sequelize.define("review", {
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }, 
+
+        comment: {
+            type: DataTypes.TEXT
+        }
+    }); 
+
+    return Review; 
+}; 
